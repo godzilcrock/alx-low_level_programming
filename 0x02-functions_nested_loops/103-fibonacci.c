ed064ed1c9d514a9926db8e39ebd7c7b5b239b4a
godzilcrock <kpobariv@gmail.com>
2022-10-19 15:47:39 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,25 +6,26 @@
  */
 int main(void)
 {
-	long int i, j, k, l;
+	int i;
+	unsigned long int j, k, l, m;
 
 	j = 1;
-
 	k = 2;
+	m = 0;
 
-	for (i = 1; i <= 50; ++i)
+	for (i = 1; i <= 33; ++i)
 	{
-		if (j != 20365011074)
+		if (j < 4000000 && (j % 2) == 0)
 		{
-			printf("%ld, ", j);
-		} else
-		{
-			printf("%ld\n", j);
+			m = m + j;
 		}
 		l = j + k;
 		j = k;
 		k = l;
 	}
+
+	printf("%lu\n", m);
+
 
 	return (0);
 }
